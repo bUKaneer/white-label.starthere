85f40c49c79f14e067cffdcbfa27f27899fc0103
Luke Baughan <luke.baughan@gmail.com>
2023-11-24 19:16:52 +0000

    Fix RUNME

--- RUNME.ps1
+++ RUNME.ps1
@@ -2,7 +2,12 @@ $GitExecutablePath = "C:\Program Files\Git\bin\git.exe"
 $DockerExecutablePath = "C:\Program Files\Docker\Docker\resources\bin\docker.exe"
 
 $RootFolder = Get-Location
+
+Write-Host "RootFolder.Path: $RootFolder"
+
 $WhiteLabelFolderPath = $RootFolder.Path + "\WhiteLabel"
+
+Write-Host "WhiteLabelFolderPath: $WhiteLabelFolderPath"
 
 if (!(Test-Path $WhiteLabelFolderPath)) {
     New-Item $WhiteLabelFolderPath -ItemType Directory
@@ -11,9 +16,10 @@ if (!(Test-Path $WhiteLabelFolderPath)) {
 Set-Location $WhiteLabelFolderPath
 
 $ContainerRegistryAndPackageManagerPath = "$WhiteLabelFolderPath\white-label.infrastructure.local-containers-and-packages"
+Write-Host "ContainerRegistryAndPackageManagerPath: $ContainerRegistryAndPackageManagerPath"
 
 if (!(Test-Path $ContainerRegistryAndPackageManagerPath)) {
-    Start-Process -FilePath $GitExecutablePath -ArgumentList "clone", "https://github.com/bUKaneer/{$ContainerRegistryAndPackageManagerPath}.git"
+    Start-Process -FilePath $GitExecutablePath -ArgumentList "clone", "https://github.com/bUKaneer/white-label.infrastructure.local-containers-and-packages.git"
 }
 
 Set-Location $ContainerRegistryAndPackageManagerPath
