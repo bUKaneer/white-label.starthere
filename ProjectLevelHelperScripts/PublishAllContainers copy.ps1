608fd42815ab47176e66921583b4bc51eaeb1b7e
Luke Baughan <luke.baughan@gmail.com>
2023-12-04 19:41:30 +0000

    Update

---
+++ ProjectLevelHelperScripts/PublishAllContainers copy.ps1
@@ -0,0 +1,11 @@
+$PublishContainerFilePaths = [System.Collections.ArrayList]@()
+$CurrentFolder = Get-Location
+
+Get-ChildItem $CurrentFolder -recurse | Where-Object { $_.Name -eq "PublishContainer.ps1" } | ForEach-Object {
+    $PublishContainerFilePaths.Add($_.Directory)
+}
+
+$PublishContainerFilePaths | ForEach-Object {
+    Set-Location $_
+    & .\PublishContainer.ps1
+}
